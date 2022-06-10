d49e03e5a733dde5f72777b83d6f9e25023a1afe
Massimiliano Giovagnoli <me@maxgio.it>
2022-06-10 10:46:21 +0200

    wip
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

--- matrix.go
+++ matrix.go
@@ -9,73 +9,69 @@ type part struct {
 }
 
 var (
-	parts = []*part{
-		&part{combinazioni: []combinazione{"A", "B"}, pointer: 0},           // part
-		&part{combinazioni: []combinazione{"1", "2", "3", "4"}, pointer: 0}, // part
-		&part{combinazioni: []combinazione{"w", "x", "y", "z"}, pointer: 0}, // part
+	parts = []part{
+		part{combinazioni: []combinazione{"A", "B"}, pointer: 0},                // part
+		part{combinazioni: []combinazione{"1", "2", "3", "4", "5"}, pointer: 0}, // part
+		part{combinazioni: []combinazione{"w", "x", "y", "z"}, pointer: 0},      // part
+		//&part{combinazioni: []combinazione{"w", "x", "y", "z"}, pointer: 0},      // part // TODO: fix this
 	}
 )
 
-// (i)	(j)
-// A	1	Z
-// B	2	Y
-// 		3	X
+// y	(i)	(j)
+// ^	A	1	Z
+// |	B	2	Y
+// |		3	X
+// ------> x
 
 // n 			= the column index
 // part.pointer	= the row index
 func main() {
-	combinazioni := []*string{}
+	combinazioni := []string{}
 	combinazione_cumulata := ""
 	y := 0
 
-	//for n, _ := range parts {
-	//	// TODO: it should start from scratch
-	//	combinazione_cumulata = ""
-	//	n = n
+	// Print
+	for _, v := range parts {
+		fmt.Println(v)
+	}
 
 	for {
 		combinazione_cumulata = ""
 
-		gotoNextPart(&combinazioni, &combinazione_cumulata, 0, parts[0], parts, y)
+		gotoNextPart(combinazioni, &combinazione_cumulata, 0, &parts[0], parts, y)
 
-		if parts[0].pointer+1 == len(parts[0].combinazioni) {
+		if parts[0].pointer == len(parts[0].combinazioni) {
 			break
 		}
 	}
 }
 
-func gotoNextPart(combinazioni *[]*string, combinazione_cumulata *string, n int, part *part, parts []*part, y int) {
+func gotoNextPart(combinazioni []string, combinazione_cumulata *string, n int, part *part, parts []part, y int) {
 
-	fmt.Println(*part)
-
-	if n+1 < len(parts) {
-		fmt.Println("avanti")
+	if n+1 < len(parts) { // Move forward until the last part is reached.
 
 		*combinazione_cumulata += string(part.combinazioni[part.pointer])
 
 		n++
-		part = parts[n]
+		part = &parts[n]
 		gotoNextPart(combinazioni, combinazione_cumulata, n, part, parts, y)
-	} else {
-		fmt.Println("indietro, sono all'ultima parte")
+	} else { // Move backward: the last part has been reached.
 
 		for _, combinazione := range part.combinazioni {
 			fmt.Println(*combinazione_cumulata + string(combinazione))
 
 			tmp := string(*combinazione_cumulata + string(combinazione))
-			*combinazioni = append(*combinazioni, &tmp)
+			combinazioni = append(combinazioni, tmp)
 		}
 
 		n--
-		part = parts[n]
+		part = &parts[n]
 
 		if part.pointer+1 < len(part.combinazioni) {
 			part.pointer++
 		} else {
-			if parts[n-1].pointer+1 < len(parts[n-1].combinazioni) {
-				part.pointer = 0
-				parts[n-1].pointer++
-			}
+			part.pointer = 0
+			parts[n-1].pointer++
 		}
 	}
 }
